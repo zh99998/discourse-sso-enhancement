02196e99fbc9ec24be98e6fb6a345525eb16a9fe
zh99998 <zh99998@gmail.com>
2016-01-28 12:27:09 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
-# name: username-localization
+# name: discourse-sso-enhancement
 # about:  https://meta.discourse.org/t/should-we-expect-avatars-to-be-propagated-using-discourse-sso-provider/35332/5?u=zh99998
 # version: 0.1
 # authors: ckamps, zh99998 <zh99998@gmail.com>
